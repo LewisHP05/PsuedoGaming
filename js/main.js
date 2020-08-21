6c7679fdfee431128da869162ed2ee9e7f5d91b2
LewisHP05 <1801100@rgu.ac.uk>
2020-08-21 20:59:21 +0100

    done for now ugh

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+const CollapseBar = {
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
+
 $(document).ready(function(){
 
     $nav = $('.nav');
@@ -13,7 +26,15 @@ $(document).ready(function(){
         loop: true,
         dots: false,
         nav: true,
-        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')] /* fucking variables ughhhhhhhhhhhhhhhh */
+        navText: [$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive: CollapseBar /* fucking variables ughhhhhhhhhhhhhhhh */
     });
+
+    /* - Scroll to top - */
+    $('.to-top span').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        },1000);
+    })  
 
 });
